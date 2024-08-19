6b6ce7bd752565955093f0e909f79a003ad38763
Sachin Kumar Saroj <sachin.uiet2025@gmail.com>
2024-08-20 03:59:42 +0530

    mehnat 2

--- frontend1/postcss.config.js
+++ frontend1/postcss.config.js
@@ -1,7 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    daisyui: {},
-  },
+module.exports = {
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("daisyui"), // Comment this out to test
+  ],
 };
