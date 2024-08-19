6a1e360291066ba83845aaca6ca56bf6fd352de9
Sachin Kumar Saroj <sachin.uiet2025@gmail.com>
2024-08-20 04:35:53 +0530

    latest

--- frontend1/tailwind.config.js
+++ frontend1/tailwind.config.js
@@ -1,12 +1,23 @@
 // tailwind.config.js
-import daisyui from "daisyui";
+// import daisyui from "daisyui";
 
+// export default {
+//   content: [
+//     "./src/**/*.{html,js}", // Update paths based on your project structure
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [daisyui],
+// };
+
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
-  content: [
-    "./src/**/*.{html,js}", // Update paths based on your project structure
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
 };
