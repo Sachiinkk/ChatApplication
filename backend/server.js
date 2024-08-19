a9508685ae652525e9119c4ba4796ebe98687e45
Sachin Kumar Saroj <sachin.uiet2025@gmail.com>
2024-08-19 05:59:13 +0530

    qwertyuio

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,9 @@ app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
