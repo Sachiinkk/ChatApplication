d785de891cb76de7b30b07453f835383e2b12794
Sachin Kumar Saroj <sachin.uiet2025@gmail.com>
2024-08-20 04:25:05 +0530

    abbbbba

--- frontend1/postcss.config.js
+++ frontend1/postcss.config.js
@@ -1,7 +1,6 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
-import daisyui from "daisyui";
 
 export default {
-  plugins: [tailwindcss(), autoprefixer(), daisyui()],
+  plugins: [tailwindcss(), autoprefixer()],
 };
--- frontend1/tailwind.config.js
+++ frontend1/tailwind.config.js
@@ -1,19 +1,8 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-// 	theme: {
-// 		extend: {},
-// 	},
-// 	// eslint-disable-next-line no-undef
-// 	plugins: [require("daisyui")],
-// };
-
-import tailwindcss from "tailwindcss";
 import daisyui from "daisyui";
 
 export default {
   content: [
-    "./src/**/*.{html,js}", // Update with your file paths
+    "./src/**/*.{html,js}", // Adjust according to your project structure
   ],
   theme: {
     extend: {},
